026a9c635e2292b79510a9b3034fabd86488c14b
DanielKoltze <danielkoltze@yahoo.dk>
2024-12-01 10:27:42 +0100

    getlist

--- day 1/script.py
+++ day 1/script.py
@@ -4,12 +4,7 @@ def assignemt1():
     file = open('day 1/file.txt', 'r')
     lines = [line.strip() for line in file]
     result = 0
-    list1 = []
-    list2 = []
-    for line in lines:
-        line = line.split(' ')
-        list1.append(int(line[0]))
-        list2.append(int(line[-1]))
+    list1, list2 = getList(lines)
     list1sorted = sorted(list1)
     list2sorted = sorted(list2)
     for i in range(len(list1sorted)):
@@ -19,12 +14,7 @@ def assignemt1():
 def assignemt2():
     file = open('day 1/file.txt', 'r')
     lines = [line.strip() for line in file]
-    list1 = []
-    list2 = []
-    for line in lines:
-        line = line.split(' ')
-        list1.append(int(line[0]))
-        list2.append(int(line[-1]))
+    list1, list2 = getList(lines)
     result = processScore(list1,list2)
     return(result)
 
@@ -37,5 +27,14 @@ def processScore(list1, list2):
                 count += 1
         result += num1 * count
     return(result)
+
+def getList(lines):
+    list1 = []
+    list2 = []
+    for line in lines:
+        line = line.split(' ')
+        list1.append(int(line[0]))
+        list2.append(int(line[-1]))
+    return list1, list2
 
 print(assignemt2())
