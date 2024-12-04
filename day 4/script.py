d51653f817a57b007af9b1b8bcf8b175e89b1e7f
DanielKoltze <danielkoltze@yahoo.dk>
2024-12-04 12:33:57 +0100

    part 2

--- day 4/script.py
+++ day 4/script.py
@@ -5,20 +5,18 @@ def assignemt1():
     result = 0
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            result += checkForWord((i,j),[(0,1),(0,2),(0,3)], matrix)
-            result += checkForWord((i,j),[(0,-1),(0,-2),(0,-3)], matrix)
-            result += checkForWord((i,j),[(1,0),(2,0),(3,0)], matrix)
-            result += checkForWord((i,j),[(-1,0),(-2,0),(-3,0)], matrix)
-            result += checkForWord((i,j),[(1,1),(2,2),(3,3)], matrix)
-            result += checkForWord((i,j),[(-1,-1),(-2,-2),(-3,-3)], matrix)
-            result += checkForWord((i,j),[(-1,1),(-2,2),(-3,3)], matrix)
-            result += checkForWord((i,j),[(1,-1),(2,-2),(3,-3)], matrix)
-
+            result += checkForWord((i,j),[(0,0),(0,1),(0,2),(0,3)], matrix,'XMAS')
+            result += checkForWord((i,j),[(0,0),(0,-1),(0,-2),(0,-3)], matrix,'XMAS')
+            result += checkForWord((i,j),[(0,0),(1,0),(2,0),(3,0)], matrix,'XMAS')
+            result += checkForWord((i,j),[(0,0),(-1,0),(-2,0),(-3,0)], matrix,'XMAS')
+            result += checkForWord((i,j),[(0,0),(1,1),(2,2),(3,3)], matrix,'XMAS')
+            result += checkForWord((i,j),[(0,0),(-1,-1),(-2,-2),(-3,-3)], matrix,'XMAS')
+            result += checkForWord((i,j),[(0,0),(-1,1),(-2,2),(-3,3)], matrix,'XMAS')
+            result += checkForWord((i,j),[(0,0),(1,-1),(2,-2),(3,-3)], matrix,'XMAS')
     return result
-def checkForWord(currentPosition, positionsToCheck, matrix):
-    WORD = 'XMAS'
+
+def checkForWord(currentPosition, positionsToCheck, matrix, WORD):
     currentY,currentX = currentPosition
-    positionsToCheck.insert(0,(0,0))
     for i, tuple in enumerate(positionsToCheck, start=0):
         addY,addX = tuple
         y = currentY + addY
@@ -33,4 +31,21 @@ def checkForWord(currentPosition, positionsToCheck, matrix):
             return 0
     return 1
 
-print(assignemt1())
+
+def assignemt2():
+    file = open('day 4/file.txt', 'r')
+    matrix = [line.strip() for line in file]
+    result = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            check = 0
+            check += checkForWord((i,j),[(-1,-1),(0,0),(1,1)], matrix,'MAS')
+            check += checkForWord((i,j),[(-1,-1),(0,0),(1,1)], matrix,'SAM')
+            check += checkForWord((i,j),[(1,-1),(0,0),(-1,1)], matrix,'MAS')
+            check += checkForWord((i,j),[(1,-1),(0,0),(-1,1)], matrix,'SAM')
+            if check == 2:
+                result += 1
+    return result
+
+print(f'Assignment 1: {assignemt1()}')
+print(f'Assignment 2: {assignemt2()}')
